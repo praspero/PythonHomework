c8f95be0c362b5eb0546767aca3715b82a383e9e
praspero <50845484+praspero@users.noreply.github.com>
2019-08-27 12:44:18 +0300

    first version

---
+++ final_task/pycalc/calculation.py
@@ -0,0 +1,345 @@
+from operators_constants import OPERATORS, ALL_OPERATORS, BINARY_OPERATORS, NUMBERS, COMPARISON_OPERATORS, CONSTANTS
+from validation import validate_input_data
+from argparser import arg_parser
+
+
+def prepare_expression_to_converting(input_formula):
+    input_formula = input_formula.lower().replace(' ', '')
+    input_formula = input_formula.replace(')(', ')*(')
+    input_formula = input_formula.replace('**', '^')
+    input_formula = input_formula.replace('(-', '(0-')
+
+    for function_name in ['pow', 'log(', 'copysign', 'ldexp', 'atan2', 'hypot', 'fmod', 'gcd']:
+        if function_name in input_formula:
+            if function_name == 'log(':
+                function_name = 'log'
+            input_formula = process_function_with_2_arguments(input_formula, function_name)
+
+    for index, element in enumerate(input_formula):
+        if element == '^' and input_formula[index + 1].isalpha():
+            input_formula = process_degree_operation(input_formula)
+            break
+
+    new_list = [input_formula[0]]
+    for element in input_formula[1:]:
+        if element == '-' and new_list[-1] == '-':
+            new_list[-1] = '+'
+        elif element == '-' and new_list[-1] == '+':
+            new_list[-1] = '-'
+        elif element == '+' and new_list[-1] == '-':
+            new_list[-1] = '-'
+        elif element == '+' and new_list[-1] == '+':
+            new_list[-1] = '+'
+        else:
+            new_list.append(element)
+    if new_list[0] == "+":
+        new_list.pop(0)
+    elif new_list[0] == '-':
+        new_list.insert(0, '0')
+
+    input_formula = "".join(new_list)
+
+    input_formula = input_formula.replace('/-', '*(0-1)/')
+    input_formula = input_formula.replace('*-', '*(0-1)*')
+    if '^-' in input_formula:
+        input_formula = process_degree_minus(input_formula)
+
+    if 'fsum' in input_formula:
+        input_formula = process_fsum(input_formula)
+
+    return input_formula
+
+
+def process_degree_operation(formula_with_degree):
+    """Function replace degree by function into degree by function with brackets
+       For example: 1^gcd(5,10) into ==> 1^(gcd(5.10))
+    """
+    expression_without_brackets = []
+    argument_checker = False
+    brackets_counter = 0
+    for index, element in enumerate(formula_with_degree):
+        if argument_checker:
+            expression_without_brackets.append(element)
+
+        if element == '(':
+            brackets_counter += 1
+        elif element == ')':
+            brackets_counter -= 1
+        elif element == '^' and formula_with_degree[index + 1].isalpha():
+            argument_checker = True
+
+        if element == ')' and not brackets_counter and argument_checker:
+            break
+
+    formula_with_degree = formula_with_degree.replace("{}".format(''.join(expression_without_brackets)),
+                                                      "({})".format(''.join(expression_without_brackets)), 1)
+
+    brackets_checker = False
+    for index, element in enumerate(formula_with_degree):
+        if element == '^' and formula_with_degree[index + 1].isalpha():
+            brackets_checker = True
+            break
+    if brackets_checker:
+        formula_with_degree = process_degree_operation(formula_with_degree)
+
+    return formula_with_degree
+
+
+def process_degree_minus(expression_with_degree_minus):
+    """Function replace negative number with degree in formula into zero with brackets
+    For example:
+        ^-10 ==> into ^(0-10)
+        ^-(7+10) == > into ^(0-(7+10))
+    """
+    expression_for_replacing = []
+    degree_minus_checker = False
+    brackets_counter = 0
+    for index, element in enumerate(expression_with_degree_minus):
+
+        if degree_minus_checker:
+            if element == ')' and not brackets_counter:
+                expression_for_replacing.append(element)
+                break
+            elif element == ')':
+                brackets_counter -= 1
+            elif element == '(':
+                brackets_counter += 1
+
+            if expression_for_replacing and (element in BINARY_OPERATORS or element in COMPARISON_OPERATORS) \
+                    and not brackets_counter:
+                break
+            else:
+                expression_for_replacing.append(element)
+
+        if element == '^' and expression_with_degree_minus[index + 1] == '-':
+            degree_minus_checker = True
+
+    expression = expression_with_degree_minus.replace(("^{}".format(''.join(expression_for_replacing))),
+                                                      "^(0{})".format(''.join(expression_for_replacing)), 1)
+
+    if '^-' in expression:
+        expression = process_degree_minus(expression)
+
+    return expression
+
+
+def process_fsum(formula_with_fsum):
+    brackets_counter = 0
+    square_brackets_counter = 0
+    arguments_counter = []
+    last_argument = []
+
+    for element in formula_with_fsum.split('fsum([', 1)[1:][0]:
+        if element == ']' and not square_brackets_counter:
+            break
+
+        if element == ',' and not brackets_counter:
+            arguments_counter.append("".join(last_argument))
+            last_argument = []
+        else:
+            last_argument.append(element)
+
+        if element == '(':
+            brackets_counter += 1
+        elif element == ')':
+            brackets_counter -= 1
+        elif element == '[':
+            square_brackets_counter += 1
+        elif element == ']':
+            square_brackets_counter -= 1
+
+    arguments_counter.append("".join(last_argument))
+
+    fsum_result = 0
+    for argument in arguments_counter:
+        fsum_result += float(calculate_reversed_polish_notation(argument))
+
+    replace_expression = "fsum([{}])".format(','.join(arguments_counter), 1)
+
+    formula_with_fsum = formula_with_fsum.replace(replace_expression, str(fsum_result), 1)
+
+    if 'fsum' in formula_with_fsum:
+        formula_with_fsum = process_fsum(formula_with_fsum)
+
+    return formula_with_fsum
+
+
+def process_function_with_2_arguments(input_formula, function_name):
+    first_argument = []
+    second_argument = []
+    first_argument_checker = True
+    bracket_checker = 0
+
+    for item in ("".join(input_formula.split(function_name)[1:])):
+        if item == '(':
+            bracket_checker += 1
+        elif item == ')':
+            bracket_checker -= 1
+        elif item == ',':
+            first_argument.pop(0)
+            first_argument_checker = False
+
+        if item == ')' and not bracket_checker and not first_argument_checker:
+            break
+        elif item == ')' and not bracket_checker:
+            first_argument.append(item)
+            break
+        elif first_argument_checker:
+            first_argument.append(item)
+        elif item != ',':
+            second_argument.append(item)
+
+    if function_name == 'log' and not second_argument:
+        second_argument.append('e')
+        replace_expression = function_name + ''.join(first_argument)
+    else:
+        replace_expression = function_name + '(' + ''.join(first_argument) + ',' + ''.join(second_argument) + ')'
+
+    x = calculate_reversed_polish_notation(''.join(first_argument))
+    y = calculate_reversed_polish_notation(''.join(second_argument))
+
+    if function_name == 'gcd':
+        x = int(x)
+        y = int(y)
+
+    final_expression = str(OPERATORS[function_name].function(x, y))
+    final_expression = input_formula.replace(replace_expression, str(final_expression), 1)
+
+    if function_name == 'log':
+        if 'log(' in final_expression:
+            final_expression = process_function_with_2_arguments(final_expression, function_name)
+    else:
+        if function_name in final_expression:
+            final_expression = process_function_with_2_arguments(final_expression, function_name)
+    return final_expression
+
+
+def break_expression_into_tokens(preparing_formula):
+    current_token = []
+    for element in preparing_formula:
+
+        if element in NUMBERS:
+            if len(current_token) > 2 and ''.join(current_token[:3]) == 'log':
+
+                current_token.append(element)
+                if ''.join(current_token) in ALL_OPERATORS:
+                    yield ''.join(current_token)
+                    current_token = []
+
+            elif current_token and (current_token[0] not in NUMBERS):
+                yield ''.join(current_token)
+                current_token = [element]
+            else:
+                current_token.append(element)
+
+        else:
+            if element in '()':
+                if current_token and (current_token[0] in NUMBERS):
+                    yield float(''.join(current_token))
+                elif current_token:
+                    yield ''.join(current_token)
+
+                current_token = [element]
+
+            elif len(current_token) > 2 and ''.join(current_token[:3]) == 'log':
+                current_token.append(element)
+                if ''.join(current_token) in ALL_OPERATORS:
+                    yield ''.join(current_token)
+                    current_token = []
+
+            elif current_token and current_token[0] in NUMBERS:
+                yield float(''.join(current_token))
+                current_token = [element]
+
+            else:
+
+                if ''.join(current_token) + element in ALL_OPERATORS:
+                    current_token.append(element)
+
+                elif ''.join(current_token) in CONSTANTS:
+                    if element == 'e' and current_token:
+                        current_token.append(element)
+                    else:
+                        yield CONSTANTS.get(''.join(current_token))
+                        current_token = [element]
+
+                elif ''.join(current_token) in ALL_OPERATORS:
+                    yield ''.join(current_token)
+                    current_token = [element]
+                else:
+                    current_token.append(element)
+
+    if current_token and (current_token[0] in NUMBERS):
+        yield float(''.join(current_token))
+    elif ''.join(current_token) in CONSTANTS:
+        yield CONSTANTS.get(''.join(current_token))
+    else:
+        yield ''.join(current_token)
+
+
+def convert_to_reversed_polish_notation(tokenize_formula):
+    stack = []
+    for token in tokenize_formula:
+        if token in CONSTANTS:
+            token = CONSTANTS.get(''.join(token))
+        if token in OPERATORS:
+            while stack and stack[-1][0] != '(' and OPERATORS[token].priority <= OPERATORS[stack[-1]].priority:
+                yield stack.pop()
+            stack.append(token)
+        elif token == ")":
+            while stack:
+                last_element = stack.pop()
+                if last_element == "(":
+                    break
+                yield last_element
+        elif token == "(":
+            stack.append(token)
+        else:
+            yield float(token)
+    while stack:
+        yield stack.pop()
+
+
+def calculate_reversed_polish_notation(input_expression):
+    if validate_input_data(input_expression):
+        return validate_input_data(input_expression)
+    if type(process_comparison_operators(input_expression)) == bool:
+        return process_comparison_operators(input_expression)
+    reversed_polish_notation = convert_to_reversed_polish_notation(break_expression_into_tokens(
+        prepare_expression_to_converting(input_expression)))
+
+    stack = []
+    for element in reversed_polish_notation:
+        if element in OPERATORS:
+            if element in BINARY_OPERATORS:
+                first_number, second_number = stack.pop(), stack.pop()
+                try:
+                    stack.append(OPERATORS[element].function(second_number, first_number))
+                except ValueError:
+                    return "ERROR: Value Error"
+            else:
+                first_number = stack.pop()
+                try:
+                    stack.append(OPERATORS[element].function(first_number))
+                except ValueError:
+                    return "ERROR: Value Error"
+        else:
+            stack.append(element)
+    precesion = 0.000001
+    if abs(stack[0] - round(stack[0])) < precesion:
+        return int(stack[0])
+    else:
+        return stack[0]
+
+
+def process_comparison_operators(formula_with_comparison):
+    for item in COMPARISON_OPERATORS:
+        if item in formula_with_comparison:
+            return COMPARISON_OPERATORS[item].function(
+                calculate_reversed_polish_notation(formula_with_comparison.split(item)[0]),
+                calculate_reversed_polish_notation(formula_with_comparison.split(item)[1]))
+    return formula_with_comparison
+
+
+user_input = arg_parser()
+print(calculate_reversed_polish_notation(user_input))
---
+++ final_task/pycalc/unit_tests.py
@@ -0,0 +1,164 @@
+import unittest
+import math
+import calculation
+from math import e, pi, cos, sin, log
+
+
+class ErrorTest(unittest.TestCase):
+    """Check Error tests"""
+
+    def test_check_error_null(self):
+        result = calculation.calculate_reversed_polish_notation('')
+        self.assertEqual(result, "ERROR: no input data")
+
+    def test_check_brackets_balance(self):
+        result = calculation.calculate_reversed_polish_notation('15*(25+1')
+        self.assertEqual(result, "ERROR: brackets aren't balanced")
+
+    def test_check_function_name(self):
+        result = calculation.calculate_reversed_polish_notation('func')
+        self.assertEqual(result, 'ERROR: unknown function namefunc')
+
+    def test_check_error_first_sign(self):
+        result = calculation.calculate_reversed_polish_notation('%1863')
+        self.assertEqual(result, "ERROR: expression starts with operator")
+
+    def test_check_error_first_sign_2(self):
+        result = calculation.calculate_reversed_polish_notation('\\1142')
+        self.assertEqual(result, "ERROR: expression starts with operator")
+
+    def test_check_error_in_param(self):
+        result = calculation.calculate_reversed_polish_notation('log(10,0,8)')
+        self.assertEqual(result, "ERROR: too many arguments in function")
+
+    def test_check_error_not_param(self):
+        result = calculation.calculate_reversed_polish_notation('hypot()')
+        self.assertEqual(result, "ERROR: too little arguments in function with 2 arguments")
+
+
+class FunctionsTest(unittest.TestCase):
+    """Check function work"""
+
+    def test_sin(self):
+        result = calculation.calculate_reversed_polish_notation('sin(pi/2)')
+        self.assertEqual(result, eval('sin(pi/2)'))
+
+    def test_acos(self):
+        result = calculation.calculate_reversed_polish_notation('acos(0.84)')
+        self.assertEqual(result, eval('math.acos(0.84)'))
+
+    def test_acosh(self):
+        result = calculation.calculate_reversed_polish_notation('acosh(1)')
+        self.assertEqual(result, eval('math.acosh(1)'))
+
+    def test_asin(self):
+        result = calculation.calculate_reversed_polish_notation('asin(0.99)')
+        self.assertEqual(result, eval('math.asin(0.99)'))
+
+    def test_asinh(self):
+        result = calculation.calculate_reversed_polish_notation('asinh(0.86)')
+        self.assertEqual(result, eval('math.asinh(0.86)'))
+
+    def test_atan(self):
+        result = calculation.calculate_reversed_polish_notation('atan(0.24)')
+        self.assertEqual(result, eval('math.atan(0.24)'))
+
+    def test_fsum_one_argument(self):
+        result = calculation.calculate_reversed_polish_notation('fsum([88])')
+        self.assertEqual(result, eval('math.fsum([88])'))
+
+    def test_fsum_two_argument(self):
+        result = calculation.calculate_reversed_polish_notation('fsum([6,3])')
+        self.assertEqual(result, eval('math.fsum([6,3])'))
+
+    def test_fsum_more_argument(self):
+        result = calculation.calculate_reversed_polish_notation('fsum([5,3,5])')
+        self.assertEqual(result, eval('math.fsum([5,3,5])'))
+
+    def test_log_2param(self):
+        result = calculation.calculate_reversed_polish_notation('log(6,7)')
+        self.assertEqual(result, eval('log(6,7)'))
+
+    def test_gcd_2param(self):
+        result = calculation.calculate_reversed_polish_notation('gcd(11,36)')
+        self.assertEqual(result, eval('math.gcd(11,36)'))
+
+    def test_ceil(self):
+        result = calculation.calculate_reversed_polish_notation('ceil(5)')
+        self.assertEqual(result, eval('math.ceil(5)'))
+
+    def test_degrees(self):
+        result = calculation.calculate_reversed_polish_notation('degrees(8)')
+        self.assertEqual(result, eval('math.degrees(8)'))
+
+
+class CalculateTest(unittest.TestCase):
+    """Check calculating"""
+
+    def test_normal(self):
+        result = calculation.calculate_reversed_polish_notation('-13')
+        self.assertEqual(result, eval('-13'))
+
+    def test_calc(self):
+        result = calculation.calculate_reversed_polish_notation('2+2*2')
+        self.assertEqual(result, eval('2+2*2'))
+
+    def test_bracket_and_minus(self):
+        result = calculation.calculate_reversed_polish_notation('6-(-13)')
+        self.assertEqual(result, eval('6-(-13)'))
+
+    def test_many_minuses(self):
+        result = calculation.calculate_reversed_polish_notation('1---1')
+        self.assertEqual(result, eval('1---1'))
+
+    def test_many_minuses_2(self):
+        result = calculation.calculate_reversed_polish_notation('-+---+-1')
+        self.assertEqual(result, eval('-+---+-1'))
+
+    def test_negative_number(self):
+        result = calculation.calculate_reversed_polish_notation('8+(-9*6-5)')
+        self.assertEqual(result, eval('8+(-9*6-5)'))
+
+    def test_del_space(self):
+        result = calculation.calculate_reversed_polish_notation('4*9 -16')
+        self.assertEqual(result, eval('4*9 -16'))
+
+    def test_log_1param(self):
+        result = calculation.calculate_reversed_polish_notation('log(1+9)')
+        self.assertEqual(result, eval('log(1+9)'))
+
+    def test_degree_function(self):
+        result = calculation.calculate_reversed_polish_notation('7^sin(5)')
+        self.assertEqual(result, eval('7**sin(5)'))
+
+    def test_degree_negative_number(self):
+        result = calculation.calculate_reversed_polish_notation('6^-1')
+        self.assertEqual(result, eval('6**(-1)'))
+
+    def test_negative_numbers(self):
+        result = calculation.calculate_reversed_polish_notation('--+-48')
+        self.assertEqual(result, eval('--+-48'))
+
+    def test_negative_numbers_multiplication(self):
+        result = calculation.calculate_reversed_polish_notation('4*-8')
+        self.assertEqual(result, eval('4*-8'))
+
+    def test_float_numbers(self):
+        result = calculation.calculate_reversed_polish_notation('8.1-9.8')
+        self.assertEqual(result, eval('8.1-9.8'))
+
+    def test_compare_function_equal(self):
+        result = calculation.calculate_reversed_polish_notation('6==5')
+        self.assertEqual(result, eval('6==5'))
+
+    def test_compare_function_not_equal(self):
+        result = calculation.calculate_reversed_polish_notation('8!=1')
+        self.assertTrue(result)
+
+    def test_complex_expression(self):
+        result = calculation.calculate_reversed_polish_notation('sin(e^log(e^e^sin(2.0),45.0) + cos(3.0+log10(e^-e)))')
+        self.assertEqual(result, eval('sin(e**log(e**e**sin(2.0),45.0) + cos(3.0+math.log10(e**-e)))'))
+
+
+if __name__ == '__main__':
+    unittest.main()
